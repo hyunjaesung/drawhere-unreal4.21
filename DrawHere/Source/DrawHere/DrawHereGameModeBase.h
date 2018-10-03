77612e9bd2a1ca8fc9e7f3e55a1396a1c0f45181
Unknown <steveyysung@gmail.com>
2018-10-03 11:23:24 +0900

    First

---
+++ DrawHere/Source/DrawHere/DrawHereGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DrawHereGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRAWHERE_API ADrawHereGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
