7d31b74bdbc9fc5f2d754a5e64a86c195f620651
Unknown <steveyysung@gmail.com>
2018-10-06 18:11:26 +0900

    instant static mesh Smoother and 0에서시작 없애기

--- DrawHere/Source/DrawHere/Stroke.cpp
+++ DrawHere/Source/DrawHere/Stroke.cpp
@@ -19,16 +19,47 @@ AStroke::AStroke()
 
 void AStroke::Update(FVector CursorLocation)
 {
-	FTransform NewStrokeTransform;
-
-	FVector LocalCursorLocation = GetTransform().InverseTransformPosition(CursorLocation);// convert to local space
-
-	NewStrokeTransform.SetLocation(LocalCursorLocation);
-
-	StrokeMeshes->AddInstance(NewStrokeTransform);
+	if (PreviousCursorLocation.IsNearlyZero()) // 0 에서 시작하는거 방지
+	{
+		PreviousCursorLocation = CursorLocation;
+		return;
+	}
 
 
+	StrokeMeshes->AddInstance(GetNextSegmentTransform(CursorLocation));
+	
 	PreviousCursorLocation = CursorLocation;
 	
+}
+
+FTransform AStroke::GetNextSegmentTransform(FVector CurrentLocation) const
+{
+
+	FTransform SegmentTranasform;
+	
+	SegmentTranasform.SetScale3D(GetNextSegmentScale(CurrentLocation));
+	SegmentTranasform.SetRotation(GetNextSegmentRotation(CurrentLocation));
+	SegmentTranasform.SetLocation(GetNextSegmentLocation(CurrentLocation));
+
+	return SegmentTranasform;
+}
+
+FVector AStroke::GetNextSegmentScale(FVector CurrentLocation) const
+{
+	FVector Segment = CurrentLocation - PreviousCursorLocation;
+
+	return FVector(Segment.Size(), 1, 1);
+}
+
+FQuat AStroke::GetNextSegmentRotation(FVector CurrentLocation) const
+{
+	FVector Segment = CurrentLocation - PreviousCursorLocation;
+	FVector SegmentNormal = Segment.GetSafeNormal();
+	return FQuat::FindBetweenNormals(FVector::ForwardVector, SegmentNormal);
+}
+
+FVector AStroke::GetNextSegmentLocation(FVector CurrentLocation) const
+{
+	return GetTransform().InverseTransformPosition(PreviousCursorLocation);
 }
 
--- DrawHere/Source/DrawHere/Stroke.h
+++ DrawHere/Source/DrawHere/Stroke.h
@@ -23,7 +23,12 @@ public:
 
 private:
 	
+	FTransform GetNextSegmentTransform(FVector CurrentLocation) const;
 	
+	FVector GetNextSegmentScale(FVector CurrentLocation) const;
+	FQuat GetNextSegmentRotation(FVector CurrentLocation) const;
+	FVector GetNextSegmentLocation(FVector CurrentLocation) const;
+
 	//Components
 	UPROPERTY(VisibleAnywhere)
 	USceneComponent * Root;
