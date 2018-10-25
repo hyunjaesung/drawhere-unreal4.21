825f82e1ddef06f8643b99b0928b7de89452909b
Unknown <steveyysung@gmail.com>
2018-10-25 20:40:44 +0900

    팔레트 메뉴만들기위해서 위젯 추가

---
+++ DrawHere/Source/DrawHere/UI/PaletteMenu/PaletteMenuHandController.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PaletteMenuHandController.h"
+
+
+APaletteMenuHandController::APaletteMenuHandController()
+{
+	PaletteMenu = CreateDefaultSubobject<UWidgetComponent>(TEXT("PaletteMenu"));
+
+	PaletteMenu->SetupAttachment(GetRootComponent());
+
+
+}
+
---
+++ DrawHere/Source/DrawHere/UI/PaletteMenu/PaletteMenuHandController.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HandControllerBase.h"
+#include "WidgetComponent.h"
+
+#include "PaletteMenuHandController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRAWHERE_API APaletteMenuHandController : public AHandControllerBase
+{
+	GENERATED_BODY()
+	
+
+public :
+	int i;
+	APaletteMenuHandController();
+
+
+private :
+	UPROPERTY(VisibleAnywhere)
+		UWidgetComponent * PaletteMenu;
+
+	
+	
+	
+};
